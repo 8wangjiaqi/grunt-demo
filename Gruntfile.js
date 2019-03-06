0e17e1f2e9b4c64a2adc0e0ad8054a8304bd086a
8wangjiaqi <1397978384@qq.com>
2019-03-06 17:30:30 +0800

    mocha-fe

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    mocha: {
+      test: {
+                src: ['test/index.html'],
+        
+      },
+  options: {
+            run: true,
+          reporter: 'Dot'
+          
+  }
+      
+    }
+             
+  });
+
+    grunt.loadNpmTasks('grunt-mocha');
+
+      grunt.registerTask('default', ['mocha']);
+
+};
---
+++ rectangle.js
@@ -0,0 +1,21 @@
+$(function(){
+  var $width=$('#width');
+  var $height=$('#height');
+  var $btnCal=$('#culate');
+  var $perimeter=$('#perimeter');
+  var $area=$('#area');
+
+
+  $btnCal.click(function(){
+    var w=$width.val(),
+        h=$height.val();
+
+    var rect=rectangle();
+
+    //var p=2*(w+h),
+    //    a=w*h;
+
+    $perimeter.val(rect.perimeter(w,h));
+    $area.val(rect.area(w,h));
+  })
+});
