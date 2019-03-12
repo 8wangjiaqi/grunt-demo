c7533a2cb66a8434df4c969bf3a74829babc3706
8wangjiaqi <1397978384@qq.com>
2019-03-12 15:25:37 +0800

    ceemin

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports= function (grunt) {
+  grunt.initConfig({
+
+       cssmin: {  
+               'rectangle.min.css': 'rectangle.css'
+             }
+                   
+   });
+
+     grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+       grunt.registerTask('default', ['cssmin']); 
+
+ };
+
---
+++ calc.js
@@ -0,0 +1,16 @@
+/* exported Rectangle */
+function Rectangle(width, height) {
+    var w = Number(width),
+              h = Number(height);
+
+    this.area = function() {
+          return w * h;
+            
+    };
+
+    this.perimeter = function() {
+          return 2 * (w + h);
+            
+    };
+
+}
