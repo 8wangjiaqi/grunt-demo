e52dd369ca406be503d00953e4970e21403947b3
8wangjiaqi <1397978384@qq.com>
2019-03-18 17:07:14 +0800

    压缩JS代码

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    uglify: {
+      release:{
+        files: {
+          'js/jquery.min.js': 'js/jquery.js'
+            
+        }              
+      }                         
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify:release']);
+}
